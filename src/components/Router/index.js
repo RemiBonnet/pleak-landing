67090b48400d57996d244191eab4aea20186d6f5
Rémi Bonnet <bonnet.rem@gmail.com>
2018-03-28 14:44:17 +0200

    refactor(Router): remove animated wrapper

--- src/components/Router/index.js
+++ src/components/Router/index.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
-import TransitionSwitch from '../../containers/TransitionSwitch'
-import AnimatedWrapper from '../../containers/AnimatedWrapper'
 import Navigation from '../../components/Navigation'
 import Home from '../../pages/Home'
 import baseStyles from '../../styles/'
@@ -12,13 +10,11 @@ class Router extends Component {
     return (
       <div>
         <Navigation />
-        <TransitionSwitch>
-          <Route
-            exact
-            path="/"
-            component={AnimatedWrapper(Home)}
-          />
-        </TransitionSwitch>
+        <Route
+          exact
+          path="/"
+          component={Home}
+        />
       </div>
     )
   }
