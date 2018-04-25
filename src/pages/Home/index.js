5cc890ea441460c899142656d5f47a269c3e9d78
Rémi Bonnet <bonnet.rem@gmail.com>
2018-04-25 11:03:40 +0200

    feat(home): remove pleak lib implementation

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,32 +4,15 @@ import Content from '../../components/Content'
 import CatchLine from '../../components/CatchLine'
 import Footer from '../../components/Footer'
 
-import { captureComponentPerfs } from '../../pleak-react-perf-monitor/lib'
-
-class Home extends Component {
-
-  constructor(props) {
-    super(props)
-
-    captureComponentPerfs(this, {
-      debug: true
-    })
-  }
-
-  render() {
-    return(
-      <div>
-        <HeroBanner />
-        <Content />
-        <CatchLine 
-          content='😱OMG, this tool sounds amazing!' />
-        <Footer />
-      </div>
-    )
-  }
-
-}
-
+const Home = (
+  <div>
+    <HeroBanner />
+    <Content />
+    <CatchLine 
+      content='😱OMG, this tool sounds amazing!' />
+    <Footer />
+  </div>
+)
 
 export default Home
 
