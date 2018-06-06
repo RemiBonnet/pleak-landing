67a295a7fb21e9fe628f962c4701bc668f51e294
Rémi Bonnet <bonnet.rem@gmail.com>
2018-06-07 00:07:43 +0200

    feat(slider): add slider animation

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -4,20 +4,17 @@ import { SliderStyle } from './style'
 
 class Slider extends Component {
 
-  state = {
-    currentSlide: 0
+  componentDidMount() {
+    // first init slider
+    this.changeSlide(0)
   }
 
   changeSlide = nbSlide => {
     let contents = document.querySelectorAll('.Slide__content')
     let slides = document.querySelectorAll('.Slide')
+    let timers = document.querySelectorAll('.Slide__content span')
 
-    this.setState({
-      currentSlide: nbSlide
-    })
-
-    let array = Array.from(slides)
-    array.forEach(slide => {
+    Array.from(slides).forEach(slide => {
       slide.style.opacity = '0'
     })
 
@@ -25,48 +22,78 @@ class Slider extends Component {
       content.classList.add('Slide__content--opacity')
     })
 
+    timers.forEach(timer => {
+      TweenLite.set(timer, {
+        opacity: 0,
+        width: 0
+      })
+    })
+
     contents[nbSlide].classList.remove('Slide__content--opacity')
     slides[nbSlide].style.opacity = '1'
+
+    TweenLite.fromTo(timers[nbSlide], 15, {
+      opacity: '1'
+    }, {
+      width: '100%',
+      onComplete: () => {
+        if (nbSlide === (slides.length - 1))
+          nbSlide = -1
+
+        this.changeSlide(nbSlide + 1)
+      }
+    })
   }
 
   render() {
+
+    let { content } = this.props
+
     return (
       <SliderStyle>
-        <div className="Slider__left">
-          <div className="Slide Slide--1"></div>
-          <div className="Slide Slide--2"></div>
-          <div className="Slide Slide--3"></div>
-        </div>
-        <div className="Slider__right">
-          <div className="Slider__right__container">
-            <div onClick={this.changeSlide.bind(this, 0)}>
-              <div className="Slide__content">
-                <span></span>
-                <svg width='28' height='28' viewBox='0 0 28 28' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                  <rect x='1.5' y='1.5' width='25' height='25' stroke='#fff' strokeWidth='3' />
-                </svg>
-                <h3>Easy to use dashboard</h3>
-                <p>Consistently track progress all your key metrics in one place with efficiency and visibility</p>
+        <div>
+          <div className="Slider__left">
+            <div className="Slide Slide--1"></div>
+            <div className="Slide Slide--2"></div>
+            <div className="Slide Slide--3"></div>
+          </div>
+          <div className="Slider__right">
+            <div className="Slider__right__container">
+              <div onClick={this.changeSlide.bind(this, 0)}>
+                <div className="Slide__content">
+                  <div className="Timer">
+                    <span></span>
+                  </div>
+                  <svg width='28' height='28' viewBox='0 0 28 28' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                    <rect x='1.5' y='1.5' width='25' height='25' stroke='#fff' strokeWidth='3' />
+                  </svg>
+                  <h3>{content[0].title}</h3>
+                  <p>{content[0].paragraph}</p>
+                </div>
               </div>
-            </div>
-            <div onClick={this.changeSlide.bind(this, 1)}>
-              <div className="Slide__content Slide__content--opacity">
-                <span></span>
-                <svg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                  <circle cx='15' cy='15' r='13.5' stroke='#fff' strokeWidth='3' />
-                </svg>
-                <h3>Real time segmentation</h3>
-                <p>Use Pleak to capture every interaction users have with your app and collect essential.</p>
+              <div onClick={this.changeSlide.bind(this, 1)}>
+                <div className="Slide__content Slide__content--opacity">
+                  <div className="Timer">
+                    <span></span>
+                  </div>
+                  <svg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                    <circle cx='15' cy='15' r='13.5' stroke='#fff' strokeWidth='3' />
+                  </svg>
+                  <h3>{content[1].title}</h3>
+                  <p>{content[1].paragraph}</p>
+                </div>
               </div>
-            </div>
-            <div onClick={this.changeSlide.bind(this, 2)}>
-              <div className="Slide__content  Slide__content--opacity">
-                <span></span>
-                <svg width='37' height='31' viewBox='0 0 37 31' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                  <path d='M17.5 0L32.6554 26.25L2.34456 26.25L17.5 0Z' transform='translate(1 3)' stroke='#fff' strokeWidth='3' />
-                </svg>
-                <h3>Get alerted on your custom flows</h3>
-                <p>Get alerted about what happened, where and when and take appropriate actions.</p>
+              <div onClick={this.changeSlide.bind(this, 2)}>
+                <div className="Slide__content Slide__content--opacity">
+                  <div className="Timer">
+                    <span></span>
+                  </div>
+                  <svg width='37' height='31' viewBox='0 0 37 31' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                    <path d='M17.5 0L32.6554 26.25L2.34456 26.25L17.5 0Z' transform='translate(1 3)' stroke='#fff' strokeWidth='3' />
+                  </svg>
+                  <h3>{content[2].title}</h3>
+                  <p>{content[2].paragraph}</p>
+               </div>
               </div>
             </div>
           </div>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,18 +1,53 @@
 import React from 'react'
-import HeroBanner from '../../components/HeroBanner'
-import Content from '../../components/Content'
-import CatchLine from '../../components/CatchLine'
-import Footer from '../../components/Footer'
+import { layoutGenerator } from 'react-break'
+import { sizes } from '../../styles/responsive.js'
 import Slider from '../../components/Slider'
+import FeaturesMobile from '../../components/FeaturesMobile'
+
+const layout = layoutGenerator({
+  mobile: 0,
+  desktop: sizes.tablet
+})
+
+const OnDesktop = layout.isAtLeast('desktop')
+const OnMobile = layout.is('mobile')
+
+const sliderContent = [
+  {
+    title: 'Easy to use dashboard',
+    paragraph: 'Consistently track progress all your key metrics in one place with efficiency and visibility.',
+    img: ''
+  },
+  {
+    title: 'Real time segmentation',
+    paragraph: 'Use Pleak to capture every interaction users have with your app and collect essential.',
+    img: ''
+  },
+  {
+    title: 'Get alerted on your custom flows',
+    paragraph: 'Get alerted about what happened, where and when and take appropriate actions.',
+    img: ''
+  },
+]
+
 
 const Home = () => (
   <div>
-    <HeroBanner />
-    <Slider />
-    <Content />
-    <CatchLine 
+    {/* <HeroBanner /> */}
+    <OnDesktop>
+      <Slider
+        content={sliderContent}
+      />
+    </OnDesktop>
+    <OnMobile>
+      <FeaturesMobile 
+        content={sliderContent}
+      />
+    </OnMobile>
+    {/* <Content /> */}
+    {/* <CatchLine 
       content='😱OMG, this tool sounds amazing!' />
-    <Footer />
+    <Footer /> */}
   </div>
 )
 
