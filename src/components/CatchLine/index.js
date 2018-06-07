24e2087b09d2c85a5150a0271bf95ef036734f25
Rémi Bonnet <bonnet.rem@gmail.com>
2018-06-07 20:40:07 +0200

    feat(blockscolor): add block github and customize dash

--- src/components/CatchLine/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { CatchLineStyle } from './style.js'
-
-const CatchLine = props => (
-  <CatchLineStyle> {props.content} </CatchLineStyle>
-)
-
-export default CatchLine
